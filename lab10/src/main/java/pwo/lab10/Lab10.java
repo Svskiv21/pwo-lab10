6322090b8502656247f23dab2148a5740b3dcedb
Svskiv21 <84392445+Svskiv21@users.noreply.github.com>
2022-12-07 19:14:48 +0100

    dodanie wszystkich plikow

---
+++ lab10/src/main/java/pwo/lab10/Lab10.java
@@ -0,0 +1,7 @@
+package pwo.lab10;
+
+public class Lab10 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ lab10/src/main/java/pwo/lab10/observer/MaxFinder.java
@@ -0,0 +1,17 @@
+package pwo.lab10.observer;
+
+public class MaxFinder implements Publisher.Subscriber {
+
+    Integer max = null;
+
+    @Override
+    public void action(int x) {
+        if (max == null || max < x) {
+            max = x;
+        }
+    }
+
+    public int getMax() {
+        return max;
+    }
+}
